065e434e748181608c8b84ca7cf99e70a2758941
simbalekerova <39839782+simbalekerova@users.noreply.github.com>
2018-12-24 02:27:13 +0600

    Update search.php

--- search.php
+++ search.php
@@ -1,10 +1,21 @@
- 
-<div class="from">
-	<form action="search_page.php" method="post">
-		<input type="text" name="search"/>
-		<button>search</button>
-	</form>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SduRateMyProfessors.com</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="all">
+        <a class="active" href="#home">Home</a>
+        <a href="http://localhost/web_project/about.html">About</a>
+        <a href="#contact">Contact</a>
 
-
- 
+         <div class="from">
+	        <form action="search_page.php" method="post">
+		        <input type="text" placeholder="Search for a professor " name="search"/>
+		        <button>Search</button>
+	        </form>
+	    </div>
+	</div>
+</body>
+</html>
